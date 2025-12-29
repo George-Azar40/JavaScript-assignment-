4ebc411e8e11b58318bedaba1e0d059597d438da
ahmed-fade <ahmad.abufarha2413.com>
2025-12-29 19:58:57 +0200

    define elements linked to index

---
+++ script/script.js
@@ -0,0 +1,29 @@
+// element edit alert
+let alertEdit = document.getElementById("confirmEdit");
+let confirmOpreationButtonEdit = document.getElementById("confirmOpreationButtonEdit");
+let cancelOpreationButtonEdit = document.getElementById("cancelOpreationButtonEdit");
+let inputEdit = document.getElementById("alertEditContentInput");
+let inputEditNote = document.getElementById("inputEditNote");
+
+//element delet alert
+let alert = document.getElementById("confirmOpreation");
+let confirmOpreationButton = document.getElementById("confirmOpreationButton");
+let cancelOpreationButton = document.getElementById("cancelOpreationButton");
+
+//elemennt add task new
+let mainInput = document.getElementById("mainInput");
+let addNewTaskButton = document.getElementById("inputButton");
+let inputNote = document.getElementById("inputNote");
+
+//add btn (All,Done,ToDo)
+let allButton = document.getElementById("all");
+let doneButton = document.getElementById("done");
+let todoButton = document.getElementById("todo");
+
+//general element (Scroll container,No Task)
+let scrollContainerDiv = document.getElementById("scrollContainer");
+let noTasksHeader = document.getElementById("noTasksHeader");
+let taskArr = document.getElementsByClassName("task");
+//btn deleeat
+let deleteDoneButton = document.getElementById("deleteDone")
+let deleteAllButton = document.getElementById("deleteAll")
