5e7c0448a2d45416ff86c6eef26df5b5c3f2c751
George-Azar40 <georgeazar456@gmail.com>
2026-01-01 16:10:37 +0300

    add button task in javascript

--- script/script.js
+++ script/script.js
@@ -273,3 +273,20 @@ const displayTasks = ()=>{
 
 
 
+
+addNewTaskButton.onclick = ()=>
+{
+    let text = mainInput.value;
+    
+    if(checkInpt(text))
+    {
+        addNewTask(text);
+        inputNote.style.display = "";
+    }
+    else
+    {
+        inputNote.style.display = "block";
+        setTimeout(()=>{inputNote.style.display = ""}, 10000)
+    }
+        
+}
